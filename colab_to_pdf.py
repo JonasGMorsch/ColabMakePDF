5f20bcce9f2de1bcdfb608842d273f23798519a6
Jonas G. Morsch <jonasgmorsch@gmail.com>
2023-08-31 04:35:44 -0300

    Update colab_to_pdf.py

--- colab_to_pdf.py
+++ colab_to_pdf.py
@@ -1,6 +1,7 @@
 # Original code from Stackoverflow
 # https://stackoverflow.com/questions/60748816/convert-google-colab-notebook-to-pdf-html
 
+!pip install ipynbname
 
 def MyFunction():
     print ('My imported function')
