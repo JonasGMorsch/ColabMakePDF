a7a60acef84ab9d5c09404bb44e1f7881b38f7c8
Jonas G. Morsch <jonasgmorsch@gmail.com>
2023-08-31 04:06:23 -0300

    Update colab_to_pdf.py

--- colab_to_pdf.py
+++ colab_to_pdf.py
@@ -1,14 +1,17 @@
 # Original code from Stackoverflow
 # https://stackoverflow.com/questions/60748816/convert-google-colab-notebook-to-pdf-html
+
+
 ###################### Check if  libary is on this runtime #####################
-try:
-    import ipynbname
-except:
-    %pip install ipynbname
-    import ipynbname
-    from google.colab import output
-    output.clear() # erase collab console
-################################################################################
+
+    try:
+        import ipynbname
+    except:
+        %pip install ipynbname
+        import ipynbname
+        from google.colab import output
+        output.clear() # erase collab console
+    ################################################################################
 
 #!apt-get install texlive texlive-xetex texlive-latex-extra pandoc
 !apt-get install texlive-xetex
