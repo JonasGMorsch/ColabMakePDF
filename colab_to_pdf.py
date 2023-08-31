12d9b2f49c856f03d403655566bdfc732a893695
Jonas G. Morsch <jonasgmorsch@gmail.com>
2023-08-31 18:56:34 -0300

    Update colab_to_pdf.py

--- colab_to_pdf.py
+++ colab_to_pdf.py
@@ -1,23 +1,24 @@
-# Original code from Stackoverflow
+# Jonas G. Morsch
+#
+# Credits to:
 # https://stackoverflow.com/questions/60748816/convert-google-colab-notebook-to-pdf-html
+# https://github.com/hurshd0/colab_notebook_to_pdf/
+
 
 def makepdf():
 
-    get_ipython().system("pip install pypandoc")
-    get_ipython().system("pip install ipynbname")
-    get_ipython().system("apt-get install texlive texlive-xetex texlive-latex-extra pandoc")
-    
-    import re, pathlib, shutil, os, ipynbname
+    get_ipython().system("pip install pypandoc --quiet")
+    get_ipython().system("pip install ipynbname --quiet")
+    get_ipython().system("apt-get install texlive texlive-xetex texlive-latex-extra pandoc --quiet")
+    import re, pathlib, os, ipynbname
     from google.colab import files, output
     
-    script_name = ipynbname.name()[7:]
-    get_ipython().system("gdown {script_name}")
+    script_id = ipynbname.name()[7:]
+    get_ipython().system("gdown {script_id}")
     
     # Get a list of all ipynb in /content/
     for filepath in pathlib.Path("/content/").iterdir():
         if re.search(r"\w+.ipynb\b", filepath.name):
-            #print(filepath)
-            output.clear() # erase collab console
-            get_ipython().system("jupyter nbconvert --to PDF '{filepath}'")
+            get_ipython().system("jupyter nbconvert --to PDF '{filepath}' --log-level ERROR")
             files.download(os.path.splitext(str(filepath))[0]+".pdf")
     return
