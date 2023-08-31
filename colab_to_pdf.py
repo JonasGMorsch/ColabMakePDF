27327a2820a5a0b22b421587d61a6951600364b7
Jonas G. Morsch <jonasgmorsch@gmail.com>
2023-08-31 14:50:56 -0300

    Update colab_to_pdf.py

--- colab_to_pdf.py
+++ colab_to_pdf.py
@@ -1,8 +1,23 @@
 # Original code from Stackoverflow
 # https://stackoverflow.com/questions/60748816/convert-google-colab-notebook-to-pdf-html
 
-!pip install ipynbname
+def makepdf():
 
-def MyFunction():
-    print ('My imported function')
+    get_ipython().system("pip install pypandoc")
+    get_ipython().system("pip install ipynbname")
+    get_ipython().system("apt-get install texlive texlive-xetex texlive-latex-extra pandoc")
+    
+    import re, pathlib, shutil, os, ipynbname
+    from google.colab import files, output
+    
+    script_name = ipynbname.name()[7:]
+    get_ipython().system("gdown {script_name}")
+    
+    # Get a list of all ipynb in /content/
+    for filepath in pathlib.Path("/content/").iterdir():
+        if re.search(r"\w+.ipynb\b", filepath.name):
+            #print(filepath)
+            output.clear() # erase collab console
+            get_ipython().system("jupyter nbconvert --to PDF '{filepath}'")
+            files.download(os.path.splitext(str(filepath))[0]+".pdf")
     return
